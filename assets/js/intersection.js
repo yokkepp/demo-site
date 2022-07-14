f858b476b97e3ee622b22d28e708a0cbd241acf3
Serita <serita@star-career.co.jp>
2022-07-14 22:12:56 +0900

    update untersection.js

--- assets/js/intersection.js
+++ assets/js/intersection.js
@@ -1,7 +1,13 @@
 "use strict";
 
-const el = document.querySelector(".slide-in-left");
+const el1 = document.querySelector(".slide-in-left");
+const el2 = document.querySelector(".slide-in-right");
+const els3 = document.querySelectorAll(".pinch-in");
 // el.classList.add("inview");
+
+const options = {
+  rootMargin: "0px 0px -150px 0px",
+};
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
@@ -13,5 +19,10 @@ const callback = (entries, observer) => {
   });
 };
 
-const io = new IntersectionObserver(callback);
-io.observe(el);
+const io = new IntersectionObserver(callback, options);
+io.observe(el1);
+io.observe(el2);
+
+els3.forEach((el) => {
+  io.observe(el);
+});
